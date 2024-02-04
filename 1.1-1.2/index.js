67a6076f5a7ab617dcc238587662e9f47f976c19
Rinat Urazaev <121090307+rinatzxc@users.noreply.github.com>
2024-02-04 21:32:50 +0300

    Add files via upload

---
+++ 1.1-1.2/index.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+  // const-definitions
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content ... />
+      <Total ... />
+    </div>
+  )
+}
+const Content = ... {
+  return (
+    <div>
+      <Part .../>
+      <Part .../>
+      <Part .../>
+    </div>
+  )
+}
+
+  return (
+    <div>
+      <h1>{course}</h1>
+      <p>
+        {part1} {exercises1}
+      </p>
+      <p>
+        {part2} {exercises2}
+      </p>
+      <p>
+        {part3} {exercises3}
+      </p>
+      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
